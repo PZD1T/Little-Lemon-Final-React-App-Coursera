2aa29cb005f436e1b156f815c0ddc8da52bd8177
PZD1T <161904089+PZD1T@users.noreply.github.com>
2025-10-01 21:28:28 -0600

    Add semantic structure, logo, nav and footer with OG meta tags

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Little Lemon. All rights reserved.</p>
+      <p>123 Main Street, Chicago, IL</p>
+    </footer>
+  );
+}
+
+export default Footer;
